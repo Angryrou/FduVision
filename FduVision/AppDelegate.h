b15f92eb3298df97ef13345f5089d78c8f1300b5
Kawhiii <chenghaolv@163.com>
2016-10-29 23:55:01 +0800

    Initial Commit

---
+++ FduVision/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FduVision
+//
+//  Created by Kawhi Lu on 2016/10/29.
+//  Copyright © 2016年 Kawhi Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FduVision/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FduVision
+//
+//  Created by Kawhi Lu on 2016/10/29.
+//  Copyright © 2016年 Kawhi Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
